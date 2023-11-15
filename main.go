36fab560025e5fa587f253c83c25aa76abea18f3
VladiQ666 <119813330+VladiQ666@users.noreply.github.com>
2023-11-15 20:40:46 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// точка входа программы
+func main() {
+	botToken := "6952553412:AAG5lQJ9Uz0CGjr-eweBCSg7YKTVcslERjs"
+	// https://api.telegram.org/bot<token>/METHOD_NAME
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
+	for {
+		updates, err := getUpdates(botUrl, offset)
+		if err != nil {
+			log.Println("Smith went wrong: ", err.Error())
+		}
+		for _, update := range updates {
+			err = respond(botUrl, update)
+			offset = update.UpdateId + 1
+		}
+		fmt.Println(updates)
+	}
+
+}
+
+// запрос обновлений
+func getUpdates(botUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates" + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var restResponse RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+	return restResponse.Result, nil
+}
+
+// ответ на обновления
+func respond(botUrl string, update Update) error {
+	var botMessage BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = update.Message.Text
+	buf, err := json.Marshal(botMessage)
+	if err != nil {
+		return err
+	}
+	_ ,err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+	return nil
+}
